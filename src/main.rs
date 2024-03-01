363d0e95ec6db75f887d72e92ddbe21b38b5b40c
mKomorek <marcin.komorek@protonmail.com>
2024-03-01 15:52:11 +0100

    log4rs introduced

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 use clap::Parser;
+use log::debug;
+use log4rs;
 
 mod arg_parser;
 
 fn main() {
+    log4rs::init_file("log4rs.yaml", Default::default())
+        .expect("Failed to load log4rs configuration");
+
     let arg_parser = arg_parser::ArgParser::parse();
-    println!(
-        "log filepath: {}",
+    debug!(
+        "Provided file path of log file: {}",
         arg_parser.log_file_path().to_str().unwrap()
     );
 }
